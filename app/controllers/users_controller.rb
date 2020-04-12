8b82bf73b0fce2cbf53f416b4e69d0697024560e
Peter Stevens <Peterstevens510@gmail.com>
2020-04-12 21:22:55 +0100

    Started to add login/logout/signup functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
-  # def index
-  
-  # end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid? 
+      redirect_to pictures_url
+    else
+      flash.now[:alert] = "Invalid email or password"
+      render :new
+    end
+    session[:user_id] = @user.id 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Rails.application.routes.draw do
-  get 'pictures/index'
+  # get 'pictures/index'
+  
+  get 'sessions/new'
 
-  resources :pictures
+  get 'sessions/create'
+
+  get 'sessions/destroy'
 
   root 'pictures#index'
+
+  resources :pictures
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new'
+  get 'delete', to: 'posts#destroy'
+
 end
 
 
